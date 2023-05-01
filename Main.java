af15fcc8cb77ba37fce9e8188387b10f5604b996
Iryna Teremenko <irina.teremenko@gmail.com>
2023-05-01 22:41:18 +0300

    Used reference to previous object

--- Main.java
+++ Main.java
@@ -1,10 +1,8 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
-        Planet p = new Planet(Planets.EARTH);
+public class Main {
+    public static void main(String[] args) {
+        Planets p = Planets.EARTH;
 
-        System.out.printf("The distance from %s to SUN :  %d km", p.planetName(), p.solarDistance());
+        System.out.printf("The distance from %s to SUN :  %d km", p.planetName(), p.getSolarDistance());
 
     }
 }
--- Planets.java
+++ Planets.java
@@ -1,59 +1,41 @@
 public enum Planets {
 
-    MERCURY(1, 0, 2439 , 0, 2),
-    VENUS(2, 50300, 6051 , 1, 3),
-    EARTH(3, 41388, 6371 , 2, 4),
-    MARS(4,78345, 3389 , 3, 5),
-    JUPITER(5,550396, 69911 , 4, 6),
-    SATURN(6,648325, 58232 , 5, 7),
-    URANUS(7,1443991, 25362 , 6, 8),
-    NEPTUNE(8,1627738, 24622 , 7, 0);
+    MERCURY(1, 0, 2439, null),
+    VENUS(2, 50300, 6051, Planets.MERCURY),
+    EARTH(3, 41388, 6371, Planets.VENUS),
+    MARS(4, 78345, 3389, Planets.EARTH),
+    JUPITER(5, 550396, 69911, Planets.MARS),
+    SATURN(6, 648325, 58232, Planets.JUPITER),
+    URANUS(7, 1443991, 25362, Planets.SATURN),
+    NEPTUNE(8, 1627738, 24622, Planets.URANUS);
 
     private int number;
     private int distance;
     private int radius;
-    private int prev;
-    private int next;
     private long solarDistance;
 
+    final long initialDistance = 57909 * 1000;
 
+    private Planets(final int number, final int distance, final int radius, final Planets prevPlanet) {
+        this.number = number;
+        this.distance = distance;
+        this.radius = radius;
 
-    private Planets(final int number, final int distance, final int radius, final int prev, final int next) {
-        this.number = number ;
-        this.distance = distance ;
-        this.radius = radius ;
-        this.prev = prev ;
-        this.next = next ;
+        if (prevPlanet != null) {
+            this.solarDistance = prevPlanet.solarDistance + distance * 1000;
+        } else {
+            this.solarDistance = initialDistance;
+        }
 
     }
 
+    public String planetName() {
+        return this.name();
+    }
 
     // Getter
     public long getSolarDistance() {
         return solarDistance;
     }
 
-    // Setter
-    public void setSolarDistance(long value) {
-        this.solarDistance = value;
-    }
-
-    public void init(){
-        Planets[] planets = Planets.values();
-        final long initialDistance = 57909*1000;
-        Planets prevPlanet = null;
-        for (Planets planet : planets) {
-
-            if (prevPlanet!=null){
-                planet.solarDistance = prevPlanet.solarDistance + planet.distance*1000;
-            }else{
-                planet.solarDistance = initialDistance;
-            }
-            prevPlanet = planet;
-
-        }
-    }
-
-
 }
-
